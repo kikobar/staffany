0ac673a36c0154eaf8069ec09ba0ab5af4eb925e
Enrique Barcelli <kiko@barcelli.net>
2026-09-26 21:01:55 +0800

    Add push functionality for staff members

--- list_staff_members.py
+++ list_staff_members.py
@@ -1,5 +1,7 @@
 import requests
 from config import *
+import json
+from push import *
 
 def list_staff_members():
     url = baseUrl+"/workspace/v2/staff"
@@ -9,8 +11,10 @@ def list_staff_members():
         "Authorization": "Bearer "+accessKey}
 
     response = requests.request("GET", url, headers=headers, data=payload)
-
-    print(response.text)
+    staff = json.loads(response.text)['data']['items']
+    for key in staff:
+        print(key)
+        push(key,staffCollection)
 
 
 
