bfc97d4506398947ebe08a6ac270f51dd08bbedb
Enrique Barcelli <kiko@barcelli.net>
2026-09-26 13:14:12 +0800

    Add list shifts functionality

---
+++ list_shifts.py
@@ -0,0 +1,31 @@
+import requests
+from config import *
+import json
+from datetime import datetime, timezone, UTC, date, timedelta
+from zoneinfo import ZoneInfo
+
+def list_shifts():
+    from_date = str(input('From date [YYYY-MM-DD]: '))
+    from_date = str(datetime.strptime(from_date, "%Y-%m-%d").replace(tzinfo=ZoneInfo(IANATimeZone)))
+    from_date = datetime.fromisoformat(from_date).timestamp()
+    from_date = datetime.fromtimestamp(from_date,tz=timezone.utc).isoformat().replace("+00:00","Z")
+    to_date = str(input('To date [YYYY-MM-DD]: '))
+    to_date = str(datetime.strptime(to_date, "%Y-%m-%d").replace(tzinfo=ZoneInfo(IANATimeZone)))
+    to_date = datetime.fromisoformat(to_date).timestamp()
+    to_date = datetime.fromtimestamp(to_date,tz=timezone.utc).isoformat().replace("+00:00","Z")
+
+    url = baseUrl+"/workspace/v2/shifts?start="+from_date+"&end="+to_date
+
+    payload = {}
+    headers = {
+        "Authorization": "Bearer "+accessKey}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
+
+
+
+if __name__ == '__main__':
+    list_shifts()
+
